981257fff5eec7bcb9a0a89cc355add5eb40bf83
isdicely <75281465+isdicely@users.noreply.github.com>
2021-04-22 11:18:16 -0700

    dropdown bar graph added

--- static/app.js
+++ static/app.js
@@ -1,23 +1,67 @@
 // Bring in samples.json with d3 and display data
 
-d3.json("data/samples.json").then(function (data) {
-  console.log(data);
-  // Use D3 to select the dropdown menu
-  var dropdownMenu = d3.selectAll("#selDataset").node();
-  // Assign the dropdown menu item ID to a variable
-  var dropdownMenuID = dropdownMenu.id;
-  // Assing the dropdown menu option to a variable
-  var selectedOption = dropdownMenu.value;
+function init() {
+  d3.json("data/samples.json").then(function (data) {
+    console.log(data);
+    // Use D3 to select the dropdown menu
+    var dropdownMenu = d3.selectAll("#selDataset").node();
+    
+    for (var i = 0; i < data.names.length; i++) {
+      var opt = document.createElement("option");
+      opt.value = i;
+      opt.innerHTML = data.names[i];
+      dropdownMenu.appendChild(opt);
+    }
+  });
+  data = [
+    {
+      type: "bar",
+      x: [1, 2, 3, 4, 5],
+      y: ["a", "b", "c", "d", "e"],
+      orientation: "h",
+      name: "Bellybutton",
+      marker: {
+        color: "rgba(190,110,240,1)",
+        width: 1,
+      },
+    },
+  ];
+  var layout = {
+    title: "Bellybutton stuff",
+  };
+  var configuration = { responsive: true };
 
-  for (var i = 0; i < data.names.length; i++) {
-    var opt = document.createElement("option");
-    opt.value = i;
-    opt.innerHTML = data.names[i];
-    dropdownMenu.appendChild(opt);
-  }
-});
+  Plotly.newPlot("bar", data, layout, configuration);
 
-// // Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
-// function dive_data (data) {
+  // Call updatePlotly() when a change takes place to the DOM
+  d3.selectAll("#selDataset").on("change", updatePlotly);
+}
 
-// }
+// This function is called when a dropdown menu item is selected
+function updatePlotly() {
+  // get data
+  d3.json("data/samples.json").then(function (data) {
+    console.log(data);
+    // Use D3 to select the dropdown menu
+    var dropdownMenu = d3.select("#selDataset");
+    // Assign the value of the dropdown menu option to a variable
+    var dataset_id = dropdownMenu.property("value");
+    var dataset = data.samples[dataset_id];
+
+    // Initialize x and y arrays
+    var x = dataset.sample_values.slice(0,10).reverse();
+    var y = dataset.otu_ids.slice(0,10).reverse().map(id => `OTU ${id}`);
+
+    
+    
+    
+    
+    // Note the extra brackets around 'x' and 'y'
+    Plotly.restyle("bar", "x", [x]);
+    Plotly.restyle("bar", "y", [y]);
+  });
+}
+
+init();
+
+// work in progress
